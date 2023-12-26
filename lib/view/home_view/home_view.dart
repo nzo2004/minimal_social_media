583219f6f25478194a0841fc54b3cdfcc3792d2b
nzo2004 <111644119+nzo2004@users.noreply.github.com>
2023-12-26 15:12:55 +0300

    Update home_view.dart

--- lib/view/home_view/home_view.dart
+++ lib/view/home_view/home_view.dart
@@ -1,4 +1,4 @@
-import 'package:clone_app/read%20data/get_user_name.dart';
+import 'package:minimal_social_media/data/get_user_name.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
